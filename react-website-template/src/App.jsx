eb453517d043e420fbf066657e04c5d598684b56
PierreKas <chikukaspierre@gmail.com>
2025-06-12 15:32:32 +0200

    correct

--- react-website-template/src/App.jsx
+++ react-website-template/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeartureSection from "./components/FeartureSection";
-import Workflow from "./components/workflow";
+import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
